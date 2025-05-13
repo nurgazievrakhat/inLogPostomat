39ae2f4af576258d81712545cd72deb5ae3fcc2f
SotaireHome <cash2u.sotaire@gmail.com>
2025-05-13 20:32:42 +0600

    SuccessFragment added

---
+++ app/src/main/java/com/example/sampleusbproject/presentation/success/SuccessFragment.kt
@@ -0,0 +1,40 @@
+package com.example.sampleusbproject.presentation.success
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.example.sampleusbproject.R
+import com.example.sampleusbproject.databinding.FragmentSuccessBinding
+
+class SuccessFragment : Fragment(R.layout.fragment_success) {
+
+    private var _binding: FragmentSuccessBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSuccessBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnToMain.setOnClickListener {
+            findNavController().navigate(
+                R.id.mainFragment
+            )
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
