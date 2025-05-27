cdf4e9de05dda530f413dfde34294697fc078c84
SotaireHome <islamov-tilek@mail.ru>
2025-05-27 15:02:45 +0600

    iv bpx added

--- app/src/main/java/com/example/sampleusbproject/presentation/cell/SelectCellViewHolder.kt
+++ app/src/main/java/com/example/sampleusbproject/presentation/cell/SelectCellViewHolder.kt
@@ -6,6 +6,7 @@ import androidx.core.graphics.ColorUtils
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.sampleusbproject.R
 import com.example.sampleusbproject.databinding.LayoutSelectCellSizeBinding
+import com.example.sampleusbproject.presentation.boards.adapter.BoardSize
 import com.example.sampleusbproject.utils.gone
 import com.example.sampleusbproject.utils.visible
 
@@ -22,7 +23,7 @@ class SelectCellViewHolder(
             binding.root.context.getString(R.string.text_cell_size_title),
             model.boardSize.name
         )
-        if (withAmount){
+        if (withAmount) {
             binding.tvAmount.text = String.format(
                 binding.root.context.getString(R.string.text_cell_amount),
                 model.boardSize.amount.toString()
@@ -33,7 +34,7 @@ class SelectCellViewHolder(
         }
         val isClickable = model.isAvailableToChoose && model.hasFreeCells()
 
-        if (isClickable){
+        if (isClickable) {
             binding.container.alpha = 1f
         } else {
             binding.container.alpha = 0.4f
@@ -43,19 +44,56 @@ class SelectCellViewHolder(
             binding.btnChoose.setImageResource(R.drawable.iv_select_cell_inactive)
             binding.tvCellState.text =
                 binding.root.context.getString(R.string.text_not_contains_free_cell)
-            binding.tvCellState.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(binding.root.context,R.color.black_30)))
+            binding.tvCellState.setTextColor(
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        binding.root.context,
+                        R.color.black_30
+                    )
+                )
+            )
             binding.root.strokeColor =
-                ContextCompat.getColor(binding.root.context,R.color.black_10)
-            binding.root.setCardBackgroundColor(ContextCompat.getColor(binding.root.context,R.color.white))
+                ContextCompat.getColor(binding.root.context, R.color.black_10)
+            binding.root.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    binding.root.context,
+                    R.color.white
+                )
+            )
         } else {
             binding.btnChoose.setImageResource(if (model.isSelected) R.drawable.iv_select_cell_active else R.drawable.iv_select_cell_inactive)
             binding.tvCellState.text =
                 binding.root.context.getString(R.string.text_contains_free_cell)
-            binding.tvCellState.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(binding.root.context,R.color.green)))
+            binding.tvCellState.setTextColor(
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        binding.root.context,
+                        R.color.green
+                    )
+                )
+            )
             binding.root.strokeColor =
-                ContextCompat.getColor(binding.root.context, if (model.isSelected) R.color.blue_baby else R.color.black_10)
-            binding.root.setCardBackgroundColor(ContextCompat.getColor(binding.root.context, if (model.isSelected) R.color.blue_light else R.color.white))
+                ContextCompat.getColor(
+                    binding.root.context,
+                    if (model.isSelected) R.color.blue_baby else R.color.black_10
+                )
+            binding.root.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    binding.root.context,
+                    if (model.isSelected) R.color.blue_light else R.color.white
+                )
+            )
         }
+
+        val imgRes = when (model.boardSize) {
+            BoardSize.S -> R.drawable.icon_box_s
+            BoardSize.M -> R.drawable.icon_box_m
+            BoardSize.L -> R.drawable.icon_box_l
+            BoardSize.XL -> R.drawable.icon_box_xl
+        }
+
+        binding.ivBox.setImageResource(imgRes)
+
         binding.root.setOnClickListener {
             if (adapterPosition != -1 && isClickable)
                 onClick(adapterPosition, model)
