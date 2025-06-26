536c0976a67b01501c939a94a8cf4e67021f2fe4
SotaireHome <islamov-tilek@mail.ru>
2025-06-26 13:08:55 +0600

    select cell with amount has fixed

--- app/src/main/java/com/example/sampleusbproject/utils/ItemTotal.kt
+++ app/src/main/java/com/example/sampleusbproject/utils/ItemTotal.kt
@@ -2,6 +2,7 @@ package com.example.sampleusbproject.utils
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.sampleusbproject.R
@@ -22,7 +23,7 @@ class ItemTotal : ConstraintLayout {
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-
+        init(attrs)
     }
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
@@ -42,8 +43,11 @@ class ItemTotal : ConstraintLayout {
             val attributes = context.obtainStyledAttributes(it, R.styleable.ItemTotal, 0, 0)
             try {
                 val title = attributes.getResourceId(R.styleable.ItemTotal_total_title, -1)
+                Log.e("dsfsdfsd", "init: $title")
                 if (title > -1)
                     binding.tvTitle.text = context.getString(title)
+            } catch (e: Exception){
+                Log.e("dsfsdfsd", "init: ${e.message}")
             } finally {
                 attributes.recycle()
             }
